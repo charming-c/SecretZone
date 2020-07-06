cdb00be8b8fb09b83c31c06c58b5dc269ef3a938
Jack <oneninsix@gmail.com>
2020-07-07 00:37:33 +0800

    delete trash

--- app/src/main/java/com/example/screctzone/MainActivity.java
+++ app/src/main/java/com/example/screctzone/MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        我是蔡加明，人送外号马良。
-        //我是黄一帆,神来之笔.
+
     }
 }
